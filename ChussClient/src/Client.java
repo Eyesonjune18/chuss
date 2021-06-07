56e3d460fd369cb35bab7d4f48f68ddd6b9b60cc
Parker-TheGhost <84059035+Parker-TheGhost@users.noreply.github.com>
2021-06-07 13:00:45 -0700

    Update Client.java
    fresher client

--- ChussClient/src/Client.java
+++ ChussClient/src/Client.java
@@ -6,38 +6,19 @@ import java.util.Scanner;
 public class Client {
     static Socket socket;
 
-//    {
-//        try {
-//            socket = new Socket("http://73.35.247.165/", 80);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
     private static OutputStream myMoveStream = null;
     private static InputStream theirMoveStream = null;
-
     private static InputStreamReader theirMoveReader;
-
-//    static {
-//        try {
-//            theirMoveReader = new InputStreamReader(socket.getInputStream());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
     private static BufferedReader theirBuffer;
-
-
     public static String myMove = null;
     public static String theirMove = null;
 
-
     public static void main(String[] args) throws IOException {
-        socket = new Socket("http://73.35.247.165/", 80);
+        socket = new Socket("73.35.247.165", 80);
         theirMoveReader = new InputStreamReader(socket.getInputStream());
         theirBuffer = new BufferedReader(theirMoveReader);
+        myMoveStream = socket.getOutputStream();
+        theirMoveStream = socket.getInputStream();
         Scanner textTest = new Scanner(System.in);
         System.out.println("Say Something");
 
@@ -45,12 +26,15 @@ public class Client {
         System.out.println("You Said: " + myMove);
 
         //myMove = "YOUR_STRING_HERE";
+
         myMoveStream.write(myMove.getBytes(Charset.forName("UTF-8")));
-        while(!theirMove.equals(String.valueOf(theirBuffer))) {
+        String bufferLine = null;
+        do {
+            bufferLine = theirBuffer.readLine();
             if(theirMoveStream != null){
-                theirMove = String.valueOf(theirBuffer);
+                theirMove = theirBuffer.readLine();
             }
-        }
+        } while(!theirMove.equals(bufferLine));
         System.out.println("They Said: " + theirMove);
     }
 }
